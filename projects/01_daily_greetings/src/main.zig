ea489f3c41516abb6fb258504926c5d1f1bf5370
AlbertUlysses <albertulysseschavez@gmail.com>
2026-07-30 17:46:38 -0700

    removing library related code

--- projects/01_daily_greetings/src/main.zig
+++ projects/01_daily_greetings/src/main.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const Io = std.Io;
 
-const daily_greetings = @import("daily_greetings");
 const time_of_day = @import("time_of_day_greeting.zig");
 
 pub fn main(init: std.process.Init) !void {
@@ -17,18 +16,4 @@ pub fn main(init: std.process.Init) !void {
     } else {
         std.debug.print("Good {s}, {s}!\n", .{ time_of_day.greeting(), args[1] });
     }
-
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
-    try daily_greetings.printAnotherMessage(stdout_writer);
-
-    try stdout_writer.flush(); // Don't forget to flush!
 }
--- projects/01_daily_greetings/src/time_of_day_greeting.zig
+++ projects/01_daily_greetings/src/time_of_day_greeting.zig
@@ -6,7 +6,14 @@ pub fn greeting() []const u8 {
         .secs = @intCast(now.toSeconds()),
     };
     // the 7 below hardcodes the PT timezone.
-    const hour = seconds.getDaySeconds().getHoursIntoDay() - 7;
+    // the addition of 5 is due to the fact that we need to get the current time
+    var hour = seconds.getDaySeconds().getHoursIntoDay();
+    if (hour < 7) {
+        hour += 24;
+    } else {
+        hour -= 7;
+    }
+
     const time_of_day = switch (hour) {
         0...11 => "morning",
         12...17 => "afternoon",
